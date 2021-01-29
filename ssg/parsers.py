0824a813281fa7d745905ec3a276c43dc2fa8086
Luiz Marques <passport@stgsys.com>
2021-01-29 16:05:22 -0300

    read/write fixes

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -13,13 +13,12 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path):
-        with open(path, "rb") as file:
-            content = file.read()
-            return content
+        with open(path, "r") as file:
+            return file.read()
 
     def write(self, path : Path, dest, content, ext=".html"):
-        full_path = self.dest / path.with_suffix(ext).name
-        with open(full_path, "wb") as file:
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
             file.write(content)
 
     def copy(self, path, source, dest):
