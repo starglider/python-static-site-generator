b0be9062d9408ddcd8b4a674147a4df1d595ee3b
Luiz Marques <passport@stgsys.com>
2021-01-29 15:58:16 -0300

    fixed function name

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -6,7 +6,7 @@ import shutil
 class Parser:
     extensions: List[str] = []
 
-    def valid_extensions(self, extension):
+    def valid_extension(self, extension):
         return extension in self.extensions
 
     def parse(self, path: Path, source: Path, dest: Path):
